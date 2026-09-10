d05b4ba2175584b379fbe96edfa57d9190f48c78
Erik C. Thauvin <erik@thauvin.net>
2026-09-09 20:16:29 -0700

    Bump Extensions Tools to version 1.3.0

--- src/bld/java/rife/bld/extension/DetektOperationBuild.java
+++ src/bld/java/rife/bld/extension/DetektOperationBuild.java
@@ -46,7 +46,7 @@ public class DetektOperationBuild extends Project {
         var junit = version(6, 1, 3);
         scope(compile)
                 .include(dependency("com.uwyn.rife2", "bld-extensions-tools",
-                        version(1, 3, 0, "SNAPSHOT")))
+                        version(1, 3, 0)))
                 .include(dependency("com.uwyn.rife2", "bld",
                         version(2, 4, 0, "SNAPSHOT")))
                 .include(dependency("io.gitlab.arturbosch.detekt", "detekt-cli",
