8758355dfdb4624afdac0b3d22f262c6f10dfcc8
Erik C. Thauvin <erik@thauvin.net>
2026-09-20 12:23:09 -0700

    Cleanup scripts

--- scripts/checkcliargs.sh
+++ scripts/checkcliargs.sh
@@ -4,7 +4,7 @@ main=dev.detekt.cli.Main
 new=/tmp/checkcliargs-new
 old=/tmp/checkcliargs-old
 
-java -cp "lib/compile/examples/lib/bld/.sandbox/bld-dokka/*" $main --help >$new
+java -cp "examples/lib/bld/.sandbox/bld-detekt/*" $main --help >$new
 java -cp "examples/lib/provided/*" $main --help >$old
 
 diff $old $new
